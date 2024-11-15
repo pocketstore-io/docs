963add17a93c7069c9799ef96ff4b4cb822add4a
Jonathan Martz <git@jonathan-martz.de>
2024-11-15 20:06:34 +0100

    update header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
+    import Fa from "svelte-fa";
     import "../tailwind.css";
+    import {
+        faCircleInfo,
+        faPlay,
+    } from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <header class="bg-white px-3 py-3">
     <section class="flex justify-between mx-auto max-w-6xl">
         <div class="logo flex items-center">
-            <img src="/pocketstore-logo.svg" class="h-12" alt="" />
+            <a href="/">
+                <img src="/pocketstore-logo.svg" class="h-12" alt="" /></a
+            >
         </div>
         <nav class="md:block hidden">
             <input
@@ -13,10 +20,18 @@
                 placeholder="Hallo Welt"
                 class="input input-neutral input-sm placeholder-white"
             />
-            <ul class="flex justify-end">
+            <ul class="flex justify-end space-x-6">
                 <li>
-                    <a href="/getting-started"> Getting Started</a>
-                    <a href="/requirements"> Requirements</a>
+                    <a href="/getting-started" class="flex">
+                        <Fa icon={faPlay} class="mt-1 mr-3" />
+                        <span class="text-black">Getting Started</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/requirements" class="flex">
+                        <Fa icon={faCircleInfo} class="mt-1 mr-3" />
+                        <span class="text-black">Requirements</span>
+                    </a>
                 </li>
             </ul>
         </nav>
